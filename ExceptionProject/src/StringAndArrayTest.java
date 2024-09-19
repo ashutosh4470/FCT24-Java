a2d7e0da0139b2a18d126429232d761cf89459cf
Ashutosh4470 <mahaleashutosh2000@gmail.com>
2024-09-19 12:00:11 +0530

    ExceptionProject

---
+++ ExceptionProject/src/StringAndArrayTest.java
@@ -0,0 +1,11 @@
+
+public class StringAndArrayTest {
+	public static void main(String[] args) {
+		String str= "Four Colors Private Limited";
+		System.out.println(str.toLowerCase());
+		System.out.println(str.toUpperCase());
+		System.out.println("str 11th letter : "+str.charAt(10));
+		System.out.println("str substring : "+str.substring(5,13));
+		System.out.println("End Main");
+	}
+}
